57b4c576de2fc0b55aaa22f5ef0f77305e6d0f3b
iT3E <26583558+iT3E@users.noreply.github.com>
2023-06-26 11:16:23 -0700

    script mods

--- scripts/custom-config-backup.sh
+++ scripts/custom-config-backup.sh
@@ -3,24 +3,24 @@
 # Description: Backup config directory and configuration commands to a USB device
 #
 
-dest=/media/usb-backup
+# dest=/media/usb-backup
 
-# Only backup if $dest is a mount
-if mountpoint -q $dest; then
-    # Backup # VyOS /config
-    backup_dest="$dest/vyos"
-    if [ ! -d "$backup_dest" ]; then
-        mkdir "$backup_dest"
-    fi
-    tar --exclude="overlay*" --exclude="unifi*" -zvcf "$backup_dest/config.$(date +%Y%m%d%H%M%S).tar.gz" /config
+# # Only backup if $dest is a mount
+# if mountpoint -q $dest; then
+#     # Backup # VyOS /config
+#     backup_dest="$dest/vyos"
+#     if [ ! -d "$backup_dest" ]; then
+#         mkdir "$backup_dest"
+#     fi
+#     tar --exclude="overlay*" --exclude="unifi*" -zvcf "$backup_dest/config.$(date +%Y%m%d%H%M%S).tar.gz" /config
 
-    # Unifi backups
-    backup_dest="$dest/unifi"
-    if [ ! -d "$backup_dest" ]; then
-        mkdir "$backup_dest"
-    fi
-    rsync -r /config/containers/unifi/data/backup/autobackup/ "$backup_dest"
+#     # Unifi backups
+#     backup_dest="$dest/unifi"
+#     if [ ! -d "$backup_dest" ]; then
+#         mkdir "$backup_dest"
+#     fi
+#     rsync -r /config/containers/unifi/data/backup/autobackup/ "$backup_dest"
 
-    # Delete backups older than 1 month
-    find $dest -type f -mtime +30 -delete
-fi
+#     # Delete backups older than 1 month
+#     find $dest -type f -mtime +30 -delete
+# fi
