0e4b33dc45bbf7ed932294115b874e7ac55a1e92
iT3E <26583558+iT3E@users.noreply.github.com>
2023-06-26 17:04:42 -0700

    bug: adding login user

--- config-parts/system.sh
+++ config-parts/system.sh
@@ -10,6 +10,12 @@ set system ipv6 disable-forwarding
 # set system login user vyos authentication public-keys personal key 'AAAAC3NzaC1lZDI1NTE5AAAAIMyYn4k4V+myBBl79Nt3t7EZugvz9A+d3ZbKyaP1w7J5'
 # set system login user vyos authentication public-keys personal type 'ssh-ed25519'
 
+
+set system login user ${SSH_VYOS_USERNAME} authentication public-keys personal key 'AAAAB3NzaC1yc2EAAAADAQABAAABgQDQ3xsijUu7JGOG+GVc4FffLRqLF4gWDRT0EWofYinTFGkzxJFhGoS76bbjCU7nUGun5YQbRS7QcWkVKfKBne/ydtc9Mm1OZ/7N3a03TNPnXeFsiNgXfrC8LQN/OKrqreXqMhnj2Hc7o/KTHR/Ui8OE1uDI3mMcETWb1hQJRaLKKMjP5n/N9rEA8tvTK48NptXl0Jf3dJecbYu4/Az+llms9csBBRK2lqGFhYrwn+a7dLYkM80NGjQkk5tIG+0HLCNwYP1vJYLKgEc/XSEvCoyP2gdE3387mMLjBwBF55aDSf/vmnIawkL6hMYCz5CMuiFl7NbxLZk4RKJwdcJiEfX5xzXEu4f3AVIA/uxyM0pTTX1lJrPL+lzKH8VMfVOFxLTuyBU2VBgcbN6J/kHKanjpjcQ4INvmCKsO7YjsGWcE8MzhWfJrFUbX0ri4hhoESk+eqfP+AgSY0x9ibT/pGD0NKJbU6M6QpV3KfbV/JZ1xgakiIJZbleRonFSL1sSX6x0='
+set system login user ${SSH_VYOS_USERNAME} authentication public-keys personal type 'ssh-rsa'
+
+delete system login user vyos
+
 set system name-server '1.1.1.1'
 
 set system sysctl parameter kernel.pty.max value '24000'
